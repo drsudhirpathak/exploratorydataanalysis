a569a5efb815599969d9f818c27570e5708e4bf8
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-10-09 11:57:26 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,8 @@
 # Coursera Data Science: Exploratory Data Analysis
 # Course project 1: Plot1
 # Sudhir Pathak
-################################################################################+## load the data
+################################################################################
+## load the data
 rm(list = ls())
 data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
 # convert the date variable to Date class
